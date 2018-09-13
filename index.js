4c5a42abfac74d2430de770d8a20b5ed2de00633
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-13 23:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ let randomNumber = Math.random();
 
 let randomInteger = Math.floor(randomNumber);
 
-let randomUserID = Math.random(1000000001, 1000000020);
+let randomUserID = randomInteger + 1000001;
